9776f1e6e4f4ce56f7913cd07e7725fac76085c9
Patryk Urban <kai_patrykurban@wales.nhs.uk>
2025-02-06 11:47:39 +0100

    Sample project

---
+++ test/WCCG.DentalMockUI.Unit.Tests/Example/UnitTest1.cs
@@ -0,0 +1,12 @@
+using FluentAssertions;
+
+namespace WCCG.DentalMockUI.Unit.Tests.Example;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        true.Should().BeTrue();
+    }
+}
