a693cc5850d85425ee63d9e948b427a6c173784c
Patryk Urban <kai_patrykurban@wales.nhs.uk>
2025-03-05 15:52:48 +0100

    Small changes

--- src/WCCG.DentalMock.UI/Program.cs
+++ src/WCCG.DentalMock.UI/Program.cs
@@ -7,6 +7,8 @@ using WCCG.DentalMock.UI.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationInsights(builder.Environment.IsDevelopment(), builder.Configuration);
+
 //EReferralsApiConfig
 builder.Services.AddOptions<EReferralsApiConfig>().Bind(builder.Configuration.GetSection(EReferralsApiConfig.SectionName));
 builder.Services.AddSingleton<IValidateOptions<EReferralsApiConfig>, ValidateEReferralsApiConfigOptions>();
@@ -16,23 +18,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => { options.OperationFilter<SwaggerProcessMessageOperationFilter>(); });
 
-builder.Services.AddApplicationInsights(builder.Environment.IsDevelopment(), builder.Configuration);
-
 builder.Services.AddHttpClients();
-
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 app.UseMiddleware<ResponseMiddleware>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
+app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
